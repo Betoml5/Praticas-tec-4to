099fb0ad80184aa7a716e59a917ec46c2d8b0b9b
Betoml5 <betoml5@hotmail.com>
2022-03-07 22:15:43 -0600

    Add binding

---
+++ ContadorDigital/Contador.cs
@@ -0,0 +1,63 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ContadorDigital
+{
+    public class Contador : INotifyPropertyChanged
+    {
+
+        int CountN;
+
+        public int Count    
+        {
+            get { return CountN; }
+            set { CountN = value; }
+       }
+
+        public ICommand SumarCommand { get; set; }
+        public ICommand RestarCommand { get; set; }
+        public ICommand ResetarCommand { get; set; }
+
+        public void Sumar()
+        {
+            CountN++;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+        }
+        public void Restar()
+        {
+
+            if (CountN == 0)
+            {
+                CountN = 0;
+            }
+            else
+            {
+                CountN--;
+            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+
+        }
+        public void Resetar()
+        {
+            CountN = 0;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+
+        }
+        public Contador()
+        {
+            SumarCommand = new RelayCommand(Sumar);
+            RestarCommand = new RelayCommand(Restar);
+            ResetarCommand = new RelayCommand(Resetar);
+        }
+
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
