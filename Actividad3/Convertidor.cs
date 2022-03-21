0acf414de082a71f1aa08f9d20970b25e79354c9
Betoml5 <betoml5@hotmail.com>
2022-03-20 18:26:20 -0600

    dummy change

---
+++ Actividad3/Convertidor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+
+namespace Actividad3
+{
+    public class Convertidor : INotifyPropertyChanged
+    {
+        public ICommand? GenerarBinarioCommand { get; set; }
+
+        public Convertidor()
+        {
+            GenerarBinarioCommand = new RelayCommand(GenerarBinario);
+        }
+
+
+        public int NumeroBinario { get; set; }
+
+        public int NumeroDecimal { get; set; }
+
+
+        public void GenerarBinario()
+        {
+            Random r = new();
+            NumeroBinario = r.Next(1, 255);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+     
+    }
+    
+
+    
+   
+}
