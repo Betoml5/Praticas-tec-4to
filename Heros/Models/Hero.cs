0cc29501d82639892344663bde94fb4fd5242b5d
Betoml5 <betoml5@hotmail.com>
2022-04-10 19:29:14 -0500

    first commit

---
+++ Heros/Models/Hero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heros.Models
+{
+    public class Hero
+    {
+        public string Name { get; set; }
+        public string History { get; set; }
+        public string Skill { get; set; }
+
+        public Hero()
+        {
+            Name = "";
+            History = "";
+            Skill = "";
+        }
+
+
+
+
+    }
+}
