ccdce32e7db0679ec9bb4b6c4a3de87d9fbfc6c7
Betoml5 <betoml5@hotmail.com>
2022-04-25 00:36:49 -0500

    Add create view and home view

---
+++ HerosApp/Models/Hero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerosApp.Models
+{
+    public class Hero
+    {
+        public string Name { get; set; }
+        public string? Age { get; set; }
+        public string Skill { get; set; }
+
+        public string? Image { get; set; }
+
+        public Hero()
+        {
+            Name = "";
+            Skill = "";
+
+        }
+    }
+}
---
+++ HerosApp/ViewModels/HeroesViewModel.cs
@@ -0,0 +1,89 @@
+﻿using GalaSoft.MvvmLight.Command;
+using HerosApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HerosApp.ViewModels
+{
+    public class HeroesViewModel : INotifyPropertyChanged
+    {
+
+        public ObservableCollection<Hero> Heroes { get; set; } = new ObservableCollection<Hero>();
+
+        public Hero? Hero { get; set; }
+        public string View { get; set; } = "home";
+
+
+
+        public ICommand ChangeViewCommand { get; set; }
+        public ICommand CancelCommand { get; set; }
+
+
+        public HeroesViewModel()
+        {
+            Open();
+            ChangeViewCommand = new RelayCommand<string>(ChangeView);
+            CancelCommand = new RelayCommand(Cancel);
+        }
+
+
+        void ChangeView(string View)
+        {
+            this.View = View;
+
+            if (View == "create")
+            {
+                Hero = new Hero();
+                
+            }
+
+            PropertyChange();
+        }
+
+        private void Cancel()
+        {
+            Hero = null;
+            ChangeView("home");
+        }
+        void Save()
+        {
+
+            var json = JsonConvert.SerializeObject(Heroes);
+            File.WriteAllText("heroes.json", json);
+        }
+
+        void Open()
+        {
+            if (File.Exists("heroes.json"))
+            {
+                var json = File.ReadAllText("heroes.json");
+                var data = JsonConvert.DeserializeObject<ObservableCollection<Hero>>(json);
+
+                if (data == null)
+                {
+                    Heroes = new ObservableCollection<Hero>();
+                }
+                else
+                {
+                    Heroes = data;
+                }
+            }
+        }
+
+        void PropertyChange(string? prop = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+    }
+}
